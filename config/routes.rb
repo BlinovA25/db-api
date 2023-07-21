27fb5ebfe704029333b1dd5055c1a846f7c7f279
Anton <anton253647@gmail.com>
2023-07-21 21:40:34 +0200

    Added info endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  # Basic models routes
   resources :notes
+
+  # Token routes
+
+  # Info routes
+  get '/info', to: 'application#info' 
+  get '/session_info', to: 'application#session_info' 
 end
