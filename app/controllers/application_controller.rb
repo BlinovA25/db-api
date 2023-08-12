c1163a565e2f8ebfafcee12a8e059f639c7a123b
Anton <anton253647@gmail.com>
2023-08-13 01:35:46 +0200

    Added reports

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,26 @@
 require 'CustomInfo.rb'
 
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
 
-    def info
-        render json:
-        { 
-            rails_version: CustomInfo.get_rails_version,
-            ruby_vesion: CustomInfo.get_ruby_version,
-            contributors: 'Anton'
-        }
-    end
+  def check_token
+    render json: "Verification fault. Token is incorrect or inpired!" unless Token.find_by(token: params[:token])&.check
+  end
 
-    def session_info
-        render json:
-        {
-            ip: request.remote_ip,
-            token: 'Coming soon...'
-        }
-    end
+  def info
+    render json:
+             {
+               rails_version: CustomInfo.get_rails_version,
+               ruby_vesion: CustomInfo.get_ruby_version,
+               contributors: 'Anton'
+             }
+  end
 
-    # def check_token
-    # end
-
+  def session_info
+    render json:
+             {
+               ip: request.remote_ip,
+               token: 'Coming soon...'
+             }
+  end
 end
